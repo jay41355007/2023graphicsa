a5988c32ea2c74ccceeb5a8db4669ed5cdaf4f2b
黃天駿 <91250218+jay41355007@users.noreply.github.com>
2023-03-15 12:02:00 +0800

    Add files via upload

---
+++ week05-1_rotate_translate/main.cpp
@@ -0,0 +1,27 @@
+#include<GL/glut.h>
+float angle =0;
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    glColor3f(0,1,0);
+    glPushMatrix();
+
+	glPopMatrix();
+	glColor3f(1,0,0);
+    glPushMatrix();
+        glRotated(angle,0,0,1);
+        glTranslatef(0.8,0,0);
+        glutSolidTeapot(0.3);
+	glPopMatrix();
+	glutSwapBuffers();
+    angle++;
+}
+int main(int argc, char *argv[])
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+	glutCreateWindow("week04");
+	glutDisplayFunc(display);
+	glutIdleFunc(display);
+	glutMainLoop();
+}
