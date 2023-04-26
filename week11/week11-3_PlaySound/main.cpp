a54b49c98dbc5bff23b553811b060e884ed8d3fe
黃天駿 <91250218+jay41355007@users.noreply.github.com>
2023-04-26 11:45:50 +0800

    Add files via upload

---
+++ week11/week11-3_PlaySound/main.cpp
@@ -0,0 +1,8 @@
+#include <windows.h>
+int main()
+{
+    PlaySound("C:\\Users\\Administrator\\Desktop\\do_re_mi\\do.wav",NULL,SND_SYNC);
+    PlaySound("C:\\Users\\Administrator\\Desktop\\do_re_mi\\do.wav",NULL,SND_SYNC);
+    PlaySound("C:\\Users\\Administrator\\Desktop\\do_re_mi\\do.wav",NULL,SND_SYNC);
+    PlaySound("C:\\Users\\Administrator\\Desktop\\do_re_mi\\do.wav",NULL,SND_SYNC);
+}
