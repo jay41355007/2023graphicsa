cf3cf48c5a48a483b4309881bc69992841d0dbad
黃天駿 <91250218+jay41355007@users.noreply.github.com>
2023-05-24 12:01:30 +0800

    Add files via upload

---
+++ week15-2_gluLookat/main.cpp
@@ -0,0 +1,31 @@
+#include <GL/glut.h>
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    glutSolidTeapot(0.3);
+    glutSwapBuffers();
+}
+void reshape(int w,int h)
+{
+    glViewport(0,0,w,h);///視窗會看到2D範圍
+    float ar=w / (float) h;///aspect ratuio長寬比
+    glMatrixMode(GL_PROJECTION);///先切換到Project矩陣
+    glLoadIdentity();///矩陣清空，成為單位矩陣(最一開始的矩陣)
+    gluPerspective(60,ar,0.01,1000);///一片空白，在茶壺裡
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(0,0,1, 0,0,0, 0,1,0);
+    glutPostRedisplay();
+}
+int main(int argc,char **argv)
+{
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_DEPTH);
+    glutCreateWindow("week15");
+
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);///老師把名字變reshape
+
+    glutMainLoop();
+}
